5bc649dc73116b7fb931cdaa51f762bb5b4f7f47
shawnotieno <otienoshawn41@gmail.com>
2025-03-19 22:06:31 +0300

    assingment done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Remove the <main> element with id "main"
+document.querySelector("main#main").remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the id of the new <h1> to "victory"
+newHeader.id = "victory";
+
+// Set the text content of the new <h1>
+newHeader.innerHTML = "Shawn is the champion"; 
+
+// Append the new <h1> to the body
+document.body.appendChild(newHeader);
